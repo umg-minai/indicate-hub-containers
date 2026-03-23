c3473caa8a513646b4b736bd26aa5b7367c9140b
Jan Moringen <jan.moringen@med.uni-greifswald.de>
2026-03-23 10:28:38 +0100

    database: Adjust concept ids to reduce collisions chance

--- database/sql/5_indicator_info.sql
+++ database/sql/5_indicator_info.sql
@@ -1,37 +1,37 @@
 INSERT INTO indicate_qi.indicator_info (concept_id, title, description)
-VALUES (2_000_000_101,
+VALUES (2_000_100_101,
         '01 Ventilation',
         'Lung-protective mechanical ventilation should reduce inflammation and lung damage. Therefore patients with ARDS should be ventilated with a tidal volume around 6 mL/kg ideal body weight (IBW). The positive end expiratory pressure (PEEP) should be adjusted according to the ARDSnet PEEP table (mild ARDS: low PEEP table, moderate/severe ARDS: high PEEP table). The plateau pressure should be lower or equal 30 cmH2O. The inspiratory pressure difference (driving pressure, Pdelta) should be lower or equal 14 cmH2O.'),
-       (2_000_000_102,
+       (2_000_100_102,
         '02 Weaning',
         'The duration of mechanical ventilation should be as short as possible.
          Daily spontaneous breathing trial seem to be important for a fast weaning.'),
-       (2_000_000_103,
+       (2_000_100_103,
         '03 Feeding',
         'Early nutrition of critically ill patients is associated with an improved outcome.
 It should start within 24 to 48 hours of admission and should be enough to meet individuals daily calorie requirement.
 The calorie target is calculated by the insulin requirement and/or phosphate measurements in a blood sample on a daily basis.'),
-       (2_000_000_104,
+       (2_000_100_104,
         '04 Glucose Control',
         'The blood glucose levels of critically ill patients should be kept within an acceptable range.
 Regular measurements, at least once per shift and possibly corrective treatments are required.
 The type of treatment differs depending on the type of derangement of blood glucose disorder, and is not included in this quality indicator.'),
-       (2_000_000_105,
+       (2_000_100_105,
         '05 Thrombembolic Prevention',
         'Venous thromboembolism (VTE) is a frequent complication that contributes significantly to morbidity and mortality among critically ill patients.
          Pharmacologic thromboprophylaxis should reduce VTE.'),
-       (2_000_000_106,
+       (2_000_100_106,
         '06 Infection Rate',
         'This quality indicator compares the incidence of ventilator associated pneumonia and catheter-related infections.'),
-       (2_000_000_107,
+       (2_000_100_107,
         '07 Pain Sedation Delir',
         'Critically ill patients on an intensive care unit are often exposed to painful stimuli.
 (Over-)sediation and Delirium is common (up to 80 % of all critically ill patients).
 Monitoring/Detection is important for early treatment and enforced by national guidelines regularly, at least once per shift.
 Due to the lack of monitoring devices the assessment of scores is critical.'),
-       (2_000_000_108,
+       (2_000_100_108,
         '08 Advanced Care Planing',
         'Critically ill patients on an intensive care unit or their relatives should be consulted regarding care/treatment plans.'),
-       (2_000_000_109,
+       (2_000_100_109,
         '09 Mobilisation',
         'TODO');
